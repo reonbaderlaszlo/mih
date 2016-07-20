d0e1184ec311239e65c60d08b9696e8174d7bf5c
Báder László <bader.laszlo22@gmail.com>
2016-07-20 16:27:41 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(function(){
-    
+    $('.order svg').on('click', function (e) {
+
+    	e.preventDefault();
+
+    	$('.order svg').removeClass('active');
+
+    	$(this).addClass('active');
+
+    	if($('.grid').hasClass('active')){
+
+    		$('.category .card-wrapper').addClass('custom-sort');
+
+    	}else{
+    		$('.category .card-wrapper').removeClass('custom-sort');
+    	}
+    })
 });

ced33a2dbfdf13545803228bb5ef279863d7cd17
Báder László <bader.laszlo22@gmail.com>
2016-07-26 16:17:05 +0200

    SIGH

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+
     $('.order svg').on('click', function (e) {
 
     	e.preventDefault();
@@ -14,5 +15,105 @@ $(function(){
     	}else{
     		$('.category .card-wrapper').removeClass('custom-sort');
     	}
-    })
+    });
+
+    $('.buttons .read').on('click', function (e) {
+
+        $('.overlay').fadeIn('fast');
+
+        $('.read.popup').fadeIn('fast');
+
+    });
+
+    $('.overlay, .popup .close').on('click', function (e) {
+
+        e.preventDefault();
+
+        $('.overlay').fadeOut('fast');
+
+        $('.popup').fadeOut('fast');
+
+    });
+
+    $('.edit-1').on('click', function (e) {
+
+        e.preventDefault();
+        
+        $(this).parent().hide();
+
+        $('.profile-data').hide();
+
+        $('.profile-data-edit').show();
+
+    });
+
+    $('.profile-data-edit .close').on('click', function (e) {
+
+        e.preventDefault();
+
+        $(this).parent().parent().hide();
+
+        $('.profile-data').show();
+
+        $('.p-d').show();
+    });
+
+    $('.edit-2').on('click', function (e) {
+
+
+        e.preventDefault();
+
+        $(this).parent().hide();
+
+        $('.introduction').hide();
+
+        $('.introduction-edit').show();
+
+    });
+
+    $('.introduction-edit .close').on('click', function (e) {
+
+        e.preventDefault();
+
+        $(this).parent().parent().hide();
+
+        $('.introduction').show();
+
+        $('.i-e').show();
+    });
+
+    $('.edit-3').on('click', function (e) {
+
+
+        e.preventDefault();
+
+        $(this).parent().hide();
+
+        $('.contact').hide();
+
+        $('.contact-edit').show();
+
+    });
+
+    $('.contact-edit .close').on('click', function (e) {
+
+        e.preventDefault();
+
+        $(this).parent().parent().hide();
+
+        $('.contact').show();
+
+        $('.c-e').show();
+    });
+
+    $('.choose-category a').on('click', function (e) {
+
+        e.preventDefault();
+        
+        $('.choose-category img').removeClass('active');
+        
+        $(this).find('img').addClass('active'); 
+
+    });
+
 });
